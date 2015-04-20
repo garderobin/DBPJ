d774a17d15f956247977cb57c60016bc1631187a
garderobin <garderobinshot@hotmail.com>
2015-04-19 22:54:02 -0400

    Convert to maven; JUnit sample.

---
+++ src/test/TestUserDAOImpl.java
@@ -0,0 +1,27 @@
+package test;
+
+import java.util.Date;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+import com.bean.User;
+import com.dao.UserDAO;
+
+public class TestUserDAOImpl extends BaseSpringContextCommon {	 
+      
+    @Autowired  
+    @Qualifier("userDAO")  
+    private UserDAO userDAO;  
+      
+    @Test   
+    public final void testSave() {  
+    	User user = new User();
+    	user.setUsername("myusername");
+        user.setEmail("myemail");      
+        user.setPassword("myjassword"); 
+        user.setTime(new Date());
+        userDAO.saveUser(user);     
+    }  
+}
