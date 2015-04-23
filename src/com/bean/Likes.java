8d147c24cbdf5eb686dda429876725e32635fcdf
dongtao111 <dongtao090908@163.com>
2015-04-23 00:13:30 -0400

    04.23.00.13

--- src/com/bean/Likes.java
+++ src/com/bean/Likes.java
@@ -1,77 +1,68 @@
 package com.bean;
 
-import java.io.Serializable;
 import java.util.Date;
 
-public class Likes implements Serializable{
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 2576733379796866068L;
-	private String username;
-	private Integer picnum;
+public class Likes{
+    private int idlikes;
+	private User user;
+	private Picture picture;
 	private Date time;
-
-	public String getUsername() {
-		return username;
+	
+	public int getIdlikes() {
+		return idlikes;
+	}
+	public void setIdlikes(int idlikes) {
+		this.idlikes = idlikes;
+	}
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
 	}
 
-	public void setUsername(String username) {
-		this.username = username;
+	public Picture getPicture() {
+		return picture;
 	}
-
-	public Integer getPicnum() {
-		return picnum;
+	public void setPicture(Picture picture) {
+		this.picture = picture;
 	}
-
-	public void setPicnum(Integer picnum) {
-		this.picnum = picnum;
-	}
-
 	public Date getTime() {
 		return time;
 	}
-
 	public void setTime(Date time) {
 		this.time = time;
 	}
-	
 	@Override
-    public int hashCode()
-    {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((username == null) ? 0 : username.hashCode());
-        result = prime * result + ((picnum == null) ? 0 : picnum.hashCode());
-        return result;
-    }
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((user == null) ? 0 : user.hashCode());
+		result = prime * result + ((picture == null) ? 0 : picture.hashCode());
+		return result;
+	}
 
     @Override
-    public boolean equals(Object obj)
-    {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Likes other = (Likes) obj;
-        if (username == null)
-        {
-            if (other.username != null)
-                return false;
-        }
-        else if (!username.equals(other.username))
-            return false;
-        if (picnum == null)
-        {
-            if (other.picnum != null)
-                return false;
-        }
-        else if (!picnum.equals(other.picnum))
-            return false;
-        return true;
-    }
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Likes other = (	Likes) obj;
+		if (user == null) {
+			if (other.user != null)
+				return false;
+		} else if (!user.equals(other.user))
+			return false;
+		if (picture == null) {
+			if (other.picture != null)
+				return false;
+		} else if (!picture.equals(other.picture))
+			return false;
+		return true;
+	}
 
 	
 }
