b3806ff40cd42296523979761a0a982948435ac8
garderobin <garderobinshot@hotmail.com>
2015-04-22 20:34:20 -0400

    JSP added.

---
+++ src/test/UserDAOImplTest.java
@@ -0,0 +1,114 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+import com.bean.Friend;
+import com.bean.PK_Friend;
+import com.bean.User;
+import com.dao.UserDAO;
+
+public class UserDAOImplTest extends BaseSpringContextCommon {
+	
+	@Autowired  
+    @Qualifier("userDAO")  
+    private UserDAO userDAO;	
+
+	
+	@Test
+	public void testFindUserByUsername() {
+		String username = "abcd";
+		assertEquals("abcd@gmail.com", userDAO.findUserByUsername(username).getEmail());
+	}
+
+	@Test
+	public void testSaveUser() {
+		User user = new User();
+    	user.setUsername("20140422");
+        user.setEmail("myemail");      
+        user.setPassword("myjassword"); 
+        user.setTime(new Date());
+        userDAO.saveUser(user);
+	}
+
+	@Test
+	public void testUpdateUser() {		
+		User user = userDAO.findUserByUsername("myusername");
+		user.setEmail("20140422@gmail.com");
+        userDAO.updateUser(user);
+        assertEquals("20140422@gmail.com", userDAO.findUserByUsername("myusername").getEmail());
+	}
+	
+	
+	@Test
+	public void testDeleteUser() {
+		User user2 = userDAO.findUserByUsername("deleteAlone");
+		userDAO.deleteUser(user2); //TODO test cascade deletion.
+	}
+	
+	
+	
+	@Test
+	public void testFindFriendsByUsername() {
+		String username = "test";
+		List<User> friends = userDAO.findFriendsByUsername(username);
+		assertEquals(2, friends.size());		
+	}
+	
+	
+	
+	@Test
+	public void testFindFriendship() {
+		Friend friend = userDAO.findFriendship("abcd", "test");
+		assertNotEquals(null, friend);
+		Friend friend2 = userDAO.findFriendship("none", "none");
+		assertEquals(null, friend2);
+	}
+	
+	
+	@Test
+	public void testAddFriend() throws ParseException {		
+		DateFormat format = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");		
+		User user1 = new User("myusername", "myjassword", 
+				"mynewemail@gmail.com", format.parse("2015-04-19 22:46:47"));
+		User user2 = new User("tousername", "myjassword", 
+				"myemail", format.parse("2015-04-21 03:46:42"));				
+		Friend friendship = new Friend(user1, user2);
+		userDAO.addFriend(friendship); // TODO try to add friend with user not exists.
+	}
+	
+	
+	@Test
+	public void testDeleteFriend() throws ParseException {		
+		DateFormat format = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");		
+		User user1 = new User("myusername", "myjassword", 
+				"mynewemail@gmail.com", format.parse("2015-04-19 22:46:47"));
+		User user2 = new User("tousername", "myjassword", 
+				"myemail", format.parse("2015-04-21 03:46:42"));				
+		Friend friendship = new Friend(user1, user2);
+		userDAO.deleteFriend(friendship); // TODO try to delete friend with user not exists.
+	}
+	
+	@Test
+	public void testDeleteFriendByIdFriend(int idfriend) {
+		//TODO
+	}
+	
+	@Test
+	public void testDeleteFriendByUsernames(String username1, String username2) {
+		//TODO
+	}
+	
+	
+
+}
---
+++ src/test/UserServiceImplTest.java
@@ -0,0 +1,54 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UserServiceImplTest {
+
+	@Test
+	public final void testSave() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testUpdate() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testLoginUser() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testUsernameExsit() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testFindFriendsByUsername() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testAddFriend() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testDeleteFriend() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testCheckFriendshipExist() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testSignInUser() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
